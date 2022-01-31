4e65a9e67d39b933164797eebf72b09fc4ac6372
Kateryna Karpova <katyakarpova@gmail.com>
2022-01-31 13:02:55 +0100

    Created components
    two ways to create components

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>First tittle</h1>
+    <h2>Second tittle</h2>
+  </div>
+</template>
+
+<!-- <script>
+      export default {
+          
+      }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style> -->
