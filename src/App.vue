ca9b8be77f75a810c7d0906fb954cce3769cabd4
Kateryna Karpova <katyakarpova@gmail.com>
2022-02-02 14:15:06 +0100

    Create StarRating component

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,27 @@
 <template>
   <div :id="$style.app">
-    <!-- <h1>{{ title }}</h1>
-    <Button @click="increment" outlined>Click me</Button> -->
-    <!-- <DirectivesTest /> -->
-    <!-- <Todos/> -->
-    <!-- <Test1/>
-    <Test2/> -->
-    <ListTodos/>
+    <StarRating :rating="4"/>
   </div>
 </template>
 
 <script>
-
-//import Button from './components/Button.vue'
-// import DirectivesTest from './components/DirectivesTest.vue'
-// import Todos from './components/Todos.vue'
-// import Test1 from './components/Test1.vue'
-// import Test2 from './components/Test2.vue'
-import ListTodos from './components/ListTodos.vue'
+import StarRating from './components/StarRating.vue'
 
 export default {
   name: 'App',
   components: {
-    //Button,
-    // DirectivesTest,
-    // Todos,
-    // Test1,
-    // Test2,
-    ListTodos
+    StarRating
   },
   data() {
     return {
-      amountOfClicks: 0
+      
     }
   },
   computed: {
-    title() {
-      return `Amount of clicks: ${this.amountOfClicks}`
-    }
+    
   }, 
   methods: {
-    increment() {
-      this.amountOfClicks += 1
-    } 
+    
   }
 }
 </script>
