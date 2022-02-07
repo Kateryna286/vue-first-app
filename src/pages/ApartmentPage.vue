734b6a9235b8075b948b716f384afeb1aa06a0be
Kateryna Karpova <katyakarpova@gmail.com>
2022-02-07 13:31:41 +0100

    New components
    Apartment info

--- src/pages/ApartmentPage.vue
+++ src/pages/ApartmentPage.vue
@@ -1,24 +1,52 @@
 <template>
-  <div>
+  <main class="apartment-page">
     <Container>
-      <h1>Apartment Page</h1>
+      <div class="apartment-page__content">
+        <ApartmentMainInfo :apartment="apartment" />
+        <ApartmentOwner class="apartment-page__owner" :owner="apartment.owner" />
+      </div>
     </Container>
-  </div>
+  </main>
 </template>
 
 <script>
 import Container from "../components/shared/Container.vue";
+import ApartmentMainInfo from "../components/apartment/ApartmentMainInfo.vue";
+import ApartmentOwner from "../components/apartment/ApartmentOwner.vue";
+import apartments from "../components/apartment/apartments";
 
 export default {
   name: "ApartmentPage",
   components: {
     Container,
+    ApartmentMainInfo,
+    ApartmentOwner,
+  },
+  computed: {
+    apartment() {
+      return apartments.find(
+        (apartment) => apartment.id === this.$route.params.id
+      );
+    },
   },
   mounted() {
-      console.log(this.$route.params.id)
-  }
+    console.log(this.apartment);
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.apartment-page {
+  padding-bottom: 55px;
+
+  &__content {
+    display: flex;
+    align-items: flex-start;
+  }
+
+  &__owner {
+      min-width: 350px;
+      margin-left: 30px;
+  }
+}
 </style>
