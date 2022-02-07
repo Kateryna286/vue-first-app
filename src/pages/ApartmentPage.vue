0f73c81c44146098bf568947e3296b12b7a99ad5
Kateryna Karpova <katyakarpova@gmail.com>
2022-02-07 14:53:49 +0100

    Created component Reviews

--- src/pages/ApartmentPage.vue
+++ src/pages/ApartmentPage.vue
@@ -3,7 +3,13 @@
     <Container>
       <div class="apartment-page__content">
         <ApartmentMainInfo :apartment="apartment" />
-        <ApartmentOwner class="apartment-page__owner" :owner="apartment.owner" />
+        <div class="apartment-page__additional-info">
+          <ApartmentOwner
+            class="apartment-page__owner"
+            :owner="apartment.owner"
+          />
+          <Reviews :reviews="reviews" />
+        </div>
       </div>
     </Container>
   </main>
@@ -13,7 +19,10 @@
 import Container from "../components/shared/Container.vue";
 import ApartmentMainInfo from "../components/apartment/ApartmentMainInfo.vue";
 import ApartmentOwner from "../components/apartment/ApartmentOwner.vue";
+import Reviews from "../components/reviews";
+
 import apartments from "../components/apartment/apartments";
+import reviews from "../components/reviews/reviews.json";
 
 export default {
   name: "ApartmentPage",
@@ -21,16 +30,17 @@ export default {
     Container,
     ApartmentMainInfo,
     ApartmentOwner,
+    Reviews,
   },
   computed: {
+    reviews() {
+      return reviews;
+    },
     apartment() {
       return apartments.find(
         (apartment) => apartment.id === this.$route.params.id
       );
     },
-  },
-  mounted() {
-    console.log(this.apartment);
   },
 };
 </script>
@@ -38,15 +48,15 @@ export default {
 <style lang="scss" scoped>
 .apartment-page {
   padding-bottom: 55px;
-
   &__content {
     display: flex;
     align-items: flex-start;
   }
-
-  &__owner {
-      min-width: 350px;
-      margin-left: 30px;
+  &__additional-info {
+    margin-left: 30px;
+    max-width: 350px;
+    flex-grow: 0;
+    flex-shrink: 1;
   }
 }
 </style>
